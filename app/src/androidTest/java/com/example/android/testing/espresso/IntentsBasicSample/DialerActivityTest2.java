6a9d83868bd49a8a1d9daff7ba03c748dcdd4767
Pavel Gorb <pavel.gorb@dataart.com>
2022-03-29 18:33:35 +0300

    Chapter4 Intents Test

--- app/src/androidTest/java/com/example/android/testing/espresso/IntentsBasicSample/DialerActivityTest2.java
+++ app/src/androidTest/java/com/example/android/testing/espresso/IntentsBasicSample/DialerActivityTest2.java
@@ -1,9 +1,8 @@
-package com.example.android.testing.espresso.IntentsBasicSampl;
+package com.example.android.testing.espresso.IntentsBasicSample;
 
-import static android.app.Instrumentation.ActivityResult;
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
@@ -11,49 +10,51 @@ import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.isInternal;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.not;
-import static org.hamcrest.core.AllOf.allOf;
 
 import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
 import android.net.Uri;
 
-import com.example.android.testing.espresso.IntentsBasicSample.DialerActivity;
-import com.example.android.testing.espresso.IntentsBasicSample.R;
-
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-@LargeTest
-public class DialerActivityTest {
+public class DialerActivityTest2 {
 
-    private static final String phoneNumber = "123-345-6789";
-    private static final Uri IntentPhoneNumber = Uri.parse("tel:" + phoneNumber);
+    private static final String PHONE_NUMBER = "123-456-7891";
+    private static final Uri INTENDED_PHONE_NUMBER = Uri.parse("tel:" + PHONE_NUMBER);
 
     @Rule
-        public IntentsTestRule<DialerActivity> mActivityRule = new IntentsTestRule<>(
-        DialerActivity.class);
+    public IntentsTestRule<DialerActivity> dialerActivityIntentsTestRule =
+            new IntentsTestRule<>(DialerActivity.class);
 
     @Before
     public void stubAllExternalIntents() {
         intending(not(isInternal()))
-            .respondWith(new ActivityResult(Activity.RESULT_OK, null));
+                .respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
     }
 
     @Test
-    public void typeNumber_ValidInput_InitiatesCall() {
+    public void callTypeNumberTest() {
         onView(withId(R.id.edit_text_caller_number))
-            .perform(typeText(phoneNumber), closeSoftKeyboard());
-        onView(withId(R.id.button_call_number)).perform(click());
+                .perform(typeText(PHONE_NUMBER));
+        closeSoftKeyboard();
+        onView(withId(R.id.button_call_number))
+                .perform(click());
 
         intended(allOf(
-            hasAction(Intent.ACTION_CALL),
-            hasData(IntentPhoneNumber)));
+                    hasAction(Intent.ACTION_CALL),
+                    hasData(INTENDED_PHONE_NUMBER)
+                )
+        );
     }
 }
